38cba00ca6b015fbacf8f7856347bd29758e35df
Nengyi Jonathan Jiang <k1334755@students.katyisd.org>
2022-01-14 10:21:55 -0600

    .

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ class Chunk{
 
     /** @param {[number,number,number,number][]} [positions] */
     constructor(positions){
-        this.blockIds =  new Uint8Array(Chunk.TOTAL_BLOCKS);
+        this.blockIds =  new Uint16Array(Chunk.TOTAL_BLOCKS);
         this.blockLight = new Uint8Array(Chunk.TOTAL_BLOCKS);
+        this.blockLightFrom = new Uint16Array(Chunk.TOTAL_BLOCKS * 3);
         this.faceLight = new Uint8Array(Chunk.TOTAL_BLOCKS * 6);
-        this.blockData = new Uint8Array(Chunk.TOTAL_BLOCKS);
+        this.blockData = new Uint16Array(Chunk.TOTAL_BLOCKS);
 
         this.setFromPositions(positions||[]);
     }
@@ -25,6 +26,7 @@ class Chunk{
     setFromPositions(positions){
         this.blockIds.fill(0);
         this.blockLight.fill(0);
+        this.blockLightFrom.fill(65535);
         this.faceLight.fill(0);
         this.blockData.fill(0);
 
@@ -44,56 +46,55 @@ class Chunk{
             for(let z = 0; z < DEPTH; z++){
                 if(this.getBlockIdAt(x,y,z) == 0){
                     this.setBlockLightAt(x,y,z,255);
-                    if(this.getBlockIdAt(x,y - 1,z) == 0)
-                        queue.push([x,y - 1,z,255]);
+                    this.setBlockLightFromAt(x,y,z,255,255,255);
+                    queue.push([x,y - 1,z,255]);
                 }
             }
         }
         
         let i = 0, MAX_UPDATES = TOTAL_BLOCKS * 64;
         while(++i < MAX_UPDATES && !queue.empty()){
-            let [x,y,z,lvl,fx,fy,fz] = queue.pop();
-
-            if(lvl <= this.getBlockLightAt(x,y,z)) continue;
-            this.setBlockLightAt(x,y,z,lvl);
+            let [x,y,z,lvl] = queue.pop();
 
             for(let [dx,dy,dz,dl] of [[0,1,0,16],[0,-1,0,0],[1,0,0,16],[-1,0,0,16],[0,0,1,16],[0,0,-1,16]]){
                 let [xx,yy,zz,ll] = [x + dx, y + dy, z + dz, lvl - dl];
-                if(this._inRange(xx,yy,zz) && this.getBlockIdAt(xx,yy,zz) == 0 && ll > this.getBlockLightAt(xx,yy,zz))
-                    queue.push([xx,yy,zz,ll,x,y,z]);
+                if(!this._inRange(xx,yy,zz)) continue;
+                if(this.getBlockIdAt(xx,yy,zz) != 0) continue;
+                if(ll > this.getBlockLightAt(xx,yy,zz))
+                    this.setBlockLightAt(xx,yy,zz,ll),
+                    this.setBlockLightFromAt(xx,yy,zz,[x,y,z]),
+                    queue.push([xx,yy,zz,ll]);
             }
         }
         
         if(log)
             if(i == MAX_UPDATES) console.warn("Too many lighting updates!");
-            else console.log("Updated lighting in " + i + steps);
+            else console.log("Updated lighting in " + i + "steps");
     }
 
     
     /** @param {[number,number,number][]} positions */
     updateLighting(positions, log=true){
-        const {WIDTH, HEIGHT, DEPTH, TOTAL_BLOCKS} = Chunk;
+        const {TOTAL_BLOCKS} = Chunk;
 
         this.blockLight.fill(0);
 
-        for(let x = 0, y = HEIGHT - 1; x < WIDTH; x++){
-            for(let z = 0; z < DEPTH; z++){
-                if(this.getBlockIdAt(x,y,z) == 0){
-                    this.setBlockLightAt(x,y,z,255);
-                    if(this.getBlockIdAt(x,y - 1,z) == 0)
-                        queue.push([x,y - 1,z,255,x,y,z]);
-                }
-            }
+        /** @type {PriorityQueue<[number,number,number,number]>*/
+        let queue = new PriorityQueue((a,b) => a[3] > b[3]);
+        for(let [x,y,z] of positions){
+            this.setBlockLightAt(x,y,z,0);
+            if(this.getBlockIdAt(x,y - 1,z) == 0)
+                queue.push([x,y - 1,z,0]);
         }
         
-        let i = 0, MAX_UPDATES = TOTAL_BLOCKS * 256;
+        let i = 0, MAX_UPDATES = TOTAL_BLOCKS * 64;
         while(++i < MAX_UPDATES && !queue.empty()){
             let [x,y,z,lvl] = queue.pop();
-            if(lvl <= this.getBlockLightAt(x,y,z)) continue;
-            this.setBlockLightAt(x,y,z,lvl);
+
             for(let [dx,dy,dz,dl] of [[0,1,0,16],[0,-1,0,0],[1,0,0,16],[-1,0,0,16],[0,0,1,16],[0,0,-1,16]]){
                 let [xx,yy,zz,ll] = [x + dx, y + dy, z + dz, lvl - dl];
                 if(this._inRange(xx,yy,zz) && this.getBlockIdAt(xx,yy,zz) == 0 && ll > this.getBlockLightAt(xx,yy,zz))
+                    this.setBlockLightAt(xx,yy,zz,ll),
                     queue.push([xx,yy,zz,ll]);
             }
         }
@@ -105,7 +106,7 @@ class Chunk{
 
     getMesh(){
         let numBlocks = this.blockIds.map(i=>i==0?0:1).reduce((a,b)=>a+b);
-        let dat = new Float32Array(positions.length * 216);
+        let dat = new Float32Array(numBlocks * 216);
         let i = 0;
         for(let [x,y,z,blockId,light] of positions){
             for(let face = 0; face < 6; face++){
@@ -120,7 +121,15 @@ class Chunk{
 
     logLighting(){
         function f(n){return " .:-=+*Q#M%@░▒▓█"[Math.floor(n / 16)].repeat(2)}
-        for(let y = Chunk.HEIGHT; y --> 0;) console.log("%c" + new Array(Chunk.WIDTH).fill(0).map((_,x)=>new Array(Chunk.DEPTH).fill(0).map((_,z)=>this.getBlockLightAt(x,y,z)).map(f).join("")).join("\n"),"font-family:monospace;")
+        for(let y = Chunk.HEIGHT; y --> 0;)
+            console.log(
+                "%c" + new Array(Chunk.WIDTH).fill(0).map(
+                    (_,x)=>new Array(Chunk.DEPTH).fill(0).map(
+                        (_,z)=>this.getBlockLightAt(x,y,z)
+                    ).map(f).join("")
+                ).join("\n"),
+                "font-family:monospace;"
+            )
     }
 
     _mapPos(x,y,z){return (x * Chunk.DEPTH + z) * Chunk.HEIGHT + y}
@@ -132,6 +141,8 @@ class Chunk{
     setBlockDataAt(x,y,z, data) {this.blockData[this._mapPos(x,y,z)] = data}
     getBlockLightAt(x,y,z){return this.blockLight[this._mapPos(x,y,z)]}
     setBlockLightAt(x,y,z, light){this.blockLight[this._mapPos(x,y,z)] = light}
+    getBlockLightFromAt(x,y,z){return [...this.blockLightFrom.slice(this._mapPos(x,y,z), this._mapPos(x,y,z) + 3)]}
+    setBlockLightFromAt(x,y,z,fx,fy,fz) {this.blockLightFrom.set([fx,fy,fz], this._mapPos(x,y,z))}
     getFaceLightAt(x,y,z,face){return this.blockLight[this._mapPos(x,y,z) * 6 + face]}
     setFaceLightAt(x,y,z,face, light){this.blockLight[this._mapPos(x,y,z) * 6 + face] = light}
 }
